f7ca46c121f7cbdf1f1ceda88edbaac613c287c2
EvanDunacusky <122305213+EvanDunacusky@users.noreply.github.com>
2023-01-27 12:43:04 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dunacusky_Evan_HW1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult AFewFavs()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            string path = _environment.WebRootPath + "/files/Evan Dunacusky Resume.pdf";
+            var stream = new FileStream(path, FileMode.Open);
+            return File(stream, "application/pdf", "Evan Dunacusky Resume.pdf");
+        }
+    }
+}
